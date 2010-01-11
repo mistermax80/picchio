6e1c9bf10c65280847d40f52858e42abad5c1fea
stmontori@libero.it <stmontori@libero.it@4987816e-cf8a-11de-8f89-a3abdf1de2f0>
2010-01-11 11:47:43 +0000

    alla grande con i servizi in stanza

---
+++ trunk/function/function_optional.php
@@ -0,0 +1,53 @@
+<?php
+include_once 'include/costant.php';
+
+function getOptional($id_booking) {
+
+	$link = mysql_connect(DB_ADDRESS,USER,PASS);
+	if (!$link) {
+		die('Could not connect: ' . mysql_error());
+	}
+	$db_selected = mysql_select_db(DB_NAME, $link);
+	if (!$db_selected) {
+		die ('Can\'t use foo : ' . mysql_error());
+	}
+
+	$query = "SELECT * FROM optional WHERE id_booking=".$id_booking;
+	//echo $query;
+	$result = mysql_query($query);
+	if (!$result) {
+		die('Invalid query: ' . mysql_error());
+	}
+	
+	$optionals = array();
+		
+	while ($row = mysql_fetch_assoc($result)) {
+		$optionals[] = $row;
+	}
+	mysql_close($link);
+	return $optionals;
+}
+
+function addOptional($id_booking,$id_product) {
+
+	$link = mysql_connect(DB_ADDRESS,USER,PASS);
+	if (!$link) {
+		die('Could not connect: ' . mysql_error());
+	}
+	$db_selected = mysql_select_db(DB_NAME, $link);
+	if (!$db_selected) {
+		die ('Can\'t use foo : ' . mysql_error());
+	}
+
+	$query = "INSERT INTO optional 
+				(id_booking,id_product)
+				VALUES
+				($id_booking,$id_product)";
+	//echo $query;
+	$result = mysql_query($query);
+	if (!$result) {
+		die('Invalid query: ' . mysql_error());
+	}
+	mysql_close($link);
+	return $id_optional;
+}
